db007d939ef02d20c34a29e02a03d7b3dfafb52e
Domenico Albanese <domealse23@gmail.com>
2026-08-28 13:49:12 +0200

    .

--- SignorYhatzee/vite.config.js
+++ SignorYhatzee/vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/SignorYahtzee/', // Sostituisci questo con il nome esatto della tua repository su GitHub
+  base: '/AlcholYhazeeApp/', // Sostituisci questo con il nome esatto della tua repository su GitHub
 })
